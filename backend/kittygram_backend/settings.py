f00a971ec36f1cabdee693986af4a25fb783b916
kostya-moshchev <kmoshchev00@mail.ru>
2024-04-04 01:37:41 +0300

    019

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,9 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-123")
 
 DEBUG = False
 
