4cc81b6ee52a1551697c37c38ecbefc74773464c
kostya-moshchev <129074721+kostya-moshchev@users.noreply.github.com>
2024-04-04 01:42:03 +0300

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-123")
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     "135.193.55.206",
